5f4a0e926d58f325594e3d7b1fb23f402ec3b59a
kyoko58 <sekikyoko58@gmail.com>
2022-07-17 13:41:26 +0900

    フッターのリンク編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#top'
 
+  get 'privacy', to: 'static_pages#privacy'
+  get 'userpolicy', to: 'static_pages#userpolicy'
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   get '/logout' => 'user_sessions#destroy'
